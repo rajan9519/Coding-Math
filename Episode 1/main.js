2ed5d9bc94ae0404597db937c3178c04fb6c1f8d
rajan singh <rajan9519@gmail.com>
2020-08-01 20:39:22 +0530

    Episode 1 codes

---
+++ Episode 1/main.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const width = (canvas.width = window.innerWidth);
+  const height = (canvas.height = window.innerHeight);
+
+  for (let i = 0; i < 100; i++) {
+    context.beginPath();
+    context.moveTo(Math.random() * width, Math.random() * height);
+    context.lineTo(Math.random() * width, Math.random() * height);
+    context.stroke();
+  }
+};
