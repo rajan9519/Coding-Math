893b9204f50120f9b2d273a991271c7157f815d3
rajan singh <rajan9519@gmail.com>
2020-08-14 13:12:53 +0530

    arctangent moving arrow

--- Episode 5/arctangent.js
+++ Episode 5/arctangent.js
@@ -3,16 +3,30 @@ window.onload = function () {
     context = canvas.getContext("2d"),
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight),
-    arrowX = width / 2,
-    arrowY = height / 2;
+    centerX = width / 2,
+    centerY = height / 2,
+    radius = 200,
+    speed = 0.02;
   let dx,
     dy,
-    angle = 0;
+    arrowX,
+    arrowY,
+    mouseX,
+    mouseY,
+    angle = 0,
+    angle2 = 0;
 
   render();
 
   function render() {
     context.clearRect(0, 0, width, height);
+
+    arrowX = centerX + Math.cos(angle2) * radius;
+    arrowY = centerY + Math.sin(angle2) * radius;
+
+    dx = mouseX - arrowX;
+    dy = mouseY - arrowY;
+    angle = Math.atan2(dy, dx);
 
     context.save();
     context.translate(arrowX, arrowY);
@@ -27,13 +41,13 @@ window.onload = function () {
     context.lineTo(10, 10);
     context.stroke();
 
+    angle2 += speed;
     context.restore();
     requestAnimationFrame(render);
   }
 
   document.body.addEventListener("mousemove", (event) => {
-    dx = event.clientX - arrowX;
-    dy = event.clientY - arrowY;
-    angle = Math.atan2(dy, dx);
+    mouseX = event.clientX;
+    mouseY = event.clientY;
   });
 };
