fb98f393e2e0884a6aa4a994ba2e91b7613b4bb5
rajan singh <rajan9519@gmail.com>
2020-08-29 18:56:45 +0530

    circle circle collision detection

---
+++ Episode 14/circleCircleCollision.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+  const canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight),
+    circle0 = {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      radius: Math.random() * 100 + 50,
+    },
+    circle1 = {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      radius: Math.random() * 100 + 50,
+    };
+
+  document.body.addEventListener("mousemove", function (event) {
+    circle1.x = event.clientX;
+    circle1.y = event.clientY;
+
+    if (utils.circleCollision(circle0, circle1)) {
+      context.fillStyle = "#f66";
+    } else {
+      context.fillStyle = "#999";
+    }
+
+    context.clearRect(0, 0, width, height);
+    context.beginPath();
+
+    context.arc(circle0.x, circle0.y, circle0.radius, 0, Math.PI * 2, false);
+    context.fill();
+
+    context.arc(circle1.x, circle1.y, circle1.radius, 0, Math.PI * 2, false);
+    context.fill();
+  });
+};
